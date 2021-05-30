c86331604e34f0b8fb11521964a03491f33729bf
zfach4 <zfach12@gmail.com>
2021-05-30 19:11:00 +0700

    nambah layouting untuk automatic dan setting logic sederhana untuk switch mode

--- app/src/main/java/com/zulfi/sema/ui/home/HomeFragment.java
+++ app/src/main/java/com/zulfi/sema/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.zulfi.sema.ui.home;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,18 +21,55 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    private Switch switchMode;
+    private View content_manual;
+    private View content_auto;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        switchMode = view.findViewById(R.id.switch_mode);
+
+        content_manual = view.findViewById(R.id.content_manual);
+        content_auto = view.findViewById(R.id.content_auto);
+
+        switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                configureContentMode(isChecked);
             }
         });
-        return root;
+
+//        homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_home, container, false);
+////        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+////                textView.setText(s);
+//            }
+//        });
+
+
+        // harusnya ini dieksekusi setelah mendapatkan data dari firebase
+
+        this.configureContentMode(switchMode.isChecked());
+        return view;
     }
+
+    private void configureContentMode(Boolean isChecked) {
+        if (isChecked) {
+            content_auto.setVisibility(View.VISIBLE);
+            content_manual.setVisibility(View.GONE);
+        } else {
+            content_auto.setVisibility(View.GONE);
+            content_manual.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/zulfi/sema/ui/home/HomeViewModel.java
+++ app/src/main/java/com/zulfi/sema/ui/home/HomeViewModel.java
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+// setting untuk http request ke firebase
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
