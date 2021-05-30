2ad34bc11eba40704b2e092c3a73e3a1bddacafc
zfach4 <zfach12@gmail.com>
2021-05-30 18:15:41 +0700

    inisial projek layouting manual mode

---
+++ app/src/main/java/com/zulfi/sema/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.zulfi.sema.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
